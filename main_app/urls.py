762e700b097ec996f10583bd331f199a20d53d7e
evan-woodworth <evan.woodworth@gmail.com>
2021-06-23 12:05:36 -0700

    models: added cat models and show functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('', views.index, name='index'),
-    path('cats/', views.cats_index, name='cats_index')
+    path('cats/', views.cats_index, name='cats_index'),
+    path('cats/<int:cat_id>/', views.cats_show, name='cats_show')
 ]
 
 
