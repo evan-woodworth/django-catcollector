ff6bdb708cae4dbb9fded23b99fad23faee98355
evan-woodworth <evan.woodworth@gmail.com>
2021-06-23 09:49:42 -0700

    views: add html and urls

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('cats/', views.cats_index, name='cats_index')
+]
+
+
---
+++ main_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', '3 legged cat', 4)
+]
+
+def cats_index(request):
+    data = {
+        'cats': cats
+    }
+    return render(request, 'cats/index.html', data)
+
+
+
